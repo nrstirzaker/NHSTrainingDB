3204e3303ba8748ea5cf0ea989dd0d011d65d7b8
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2019-06-16 09:26:46 +0100

    Added delete API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const mongodb = require('./datasource.js');
+const mongoClient = require('./datasource.js');
+var mongodb = require('mongodb')
 var config = require('./config');
 const port = config.server.port;
 
@@ -11,6 +12,9 @@ var db
 
 app.get('/', (req, res) => res.send('Server is running'))
 
+
+// --- training ---
+
 app.get('/training', function (req, res) {
     db.collection('training').find().toArray(function (err, results) {
         if (err) {
@@ -20,8 +24,9 @@ app.get('/training', function (req, res) {
     })
 })
 
-app.post('/role', function (req, res) {
-    db.collection('role').insertOne(req.body, function (err, result) {
+app.delete('/training/:id', function (req, res) {
+    console.log("training:delete:id: " + req.params.id);
+    db.collection('training').deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function (err, result) {
         if (err) {
             return console.log(err)
         }
@@ -29,6 +34,16 @@ app.post('/role', function (req, res) {
     });
 });
 
+app.post('/training', function (req, res) {
+    console.log("training:insert: " + req.body);
+    db.collection('training').insertOne(req.body, function (err, result) {
+        if (err) {
+            return console.log(err)
+        }
+        res.sendStatus(200);
+    });
+});
+// --- role ---
 app.get('/role', function (req, res) {
     db.collection('role').find().toArray(function (err, results) {
         if (err) {
@@ -39,6 +54,7 @@ app.get('/role', function (req, res) {
 })
 
 app.post('/role', function (req, res) {
+    console.log("role:insert: " + req.body);
     db.collection('role').insertOne(req.body, function (err, result) {
         if (err) {
             return console.log(err)
@@ -46,6 +62,18 @@ app.post('/role', function (req, res) {
         res.sendStatus(200);
     });
 });
+
+app.delete('/role/:id', function (req, res) {
+    console.log("role:delete:id: " + req.params.id);
+    db.collection('role').deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function (err, result) {
+        if (err) {
+            return console.log(err)
+        }
+        res.sendStatus(200);
+    });
+});
+
+// --- equipment ---
 
 app.get('/equipment', function (req, res) {
     db.collection('equipment').find().toArray(function (err, results) {
@@ -57,6 +85,7 @@ app.get('/equipment', function (req, res) {
 })
 
 app.post('/equipment', function (req, res) {
+    console.log("equipment:insert: " + req.body);
     db.collection('equipment').insertOne(req.body, function (err, result) {
         if (err) {
             return console.log(err)
@@ -64,6 +93,18 @@ app.post('/equipment', function (req, res) {
         res.sendStatus(200);
     });
 });
+
+app.delete('/equipment/:id', function (req, res) {
+    console.log("equipment:delete:id: " + req.params.id);
+    db.collection('equipment').deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function (err, result) {
+        if (err) {
+            return console.log(err)
+        }
+        res.sendStatus(200);
+    });
+});
+
+// --- staff --- 
 
 app.get('/staff', function (req, res) {
     db.collection('staff').find().toArray(function (err, results) {
@@ -75,6 +116,7 @@ app.get('/staff', function (req, res) {
 })
 
 app.post('/staff', function (req, res) {
+    console.log("staff:insert: " + req.body);
     db.collection('staff').insertOne(req.body, function (err, result) {
         if (err) {
             return console.log(err)
@@ -83,9 +125,19 @@ app.post('/staff', function (req, res) {
     });
 });
 
-mongodb.connectToServer(function (err) {
+app.delete('/staff/:id', function (req, res) {
+    console.log("delete id: " + req.params.id);
+    db.collection('staff').deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function (err, result) {
+        if (err) {
+            return console.log(err)
+        }
+        res.sendStatus(200);
+    });
+});
+
+mongoClient.connectToServer(function (err) {
     app.listen(port, function () {
         console.log('Node server listening on ' + port);
-        db = mongodb.getDb();
+        db = mongoClient.getDb();
     })
 });
